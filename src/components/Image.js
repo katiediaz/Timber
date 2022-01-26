f26dda201df439133c0f66a7e15d05805eae1dcf
Katie <diazkatie@gmail.com>
2022-01-26 14:55:28 -0800

    Image shows up on page

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,9 +1,23 @@
-import React from 'react';
-import './Image.css';
+import React from "react";
+import "./Image.css";
+
 const Image = () => {
-  return ([
-  <p><a href ='https://www.wta.org/site_images/hikes/dscn9597.jpg-1/@@images/777894df-956f-4e76-8302-875cba4ffaa7.jpeg'> Oyster Dome </a></p>,
-  <button> Next Hike </button>]);
+  return (
+    <div className="image">
+      <p>
+        <img
+          src="https://www.wta.org/site_images/hikes/dscn9597.jpg-1/@@images/777894df-956f-4e76-8302-875cba4ffaa7.jpeg"
+          alt="Oyster Dome"
+          width="650"
+          height="500"
+        ></img>
+        <div className="reactions">
+          <button>Nope</button>
+          <button>Get Hiking</button>
+        </div>
+      </p>
+    </div>
+  );
 };
 
 export default Image;
