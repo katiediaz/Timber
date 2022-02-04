d86918305ba39d5e2f344c2d45ddfd29521181be
Katie <diazkatie@gmail.com>
2022-02-04 10:49:55 -0800

    Firestore added and liked hikes route added

--- src/App.js
+++ src/App.js
@@ -6,25 +6,15 @@ import HikeViewer from "./components/HikeViewer";
 import SwipeButtons from "./components/SwipeButtons";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-
 function App() {
-  
-
   return (
     <div className="App">
-      <Header />
       <Router>
+        <Header />
         <Routes>
-          <Route path="/about" element={<h1>AboutPage</h1>}>
-          </Route>
-          <Route
-            path="/" element={
-             <HikeViewer/>
-            }
-          ></Route>
-          <Route>
-            
-          </Route>
+          <Route path="/liked" element={<h1>Liked Hikes</h1>}></Route>
+          <Route path="/about" element={<h1>AboutPage</h1>}></Route>
+          <Route path="/" element={<HikeViewer />}></Route>
         </Routes>
       </Router>
     </div>
